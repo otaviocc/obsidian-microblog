bd3259945c83fa6b9d707c228453dd64fd47c948
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2022-09-22 12:00:25 +0200

    Apply cosmetic changes

--- source/NetworkRequest.ts
+++ source/NetworkRequest.ts
@@ -5,7 +5,12 @@ export class NetworkRequest {
     readonly method: string
     readonly body?: string
     
-    constructor(path: string, parameters: Record<string, string>, method: string, body?: string) {
+    constructor(
+        path: string,
+        parameters: Record<string, string>,
+        method: string,
+        body?: string
+    ) {
         this.path = path
         this.parameters = parameters
         this.body = body
