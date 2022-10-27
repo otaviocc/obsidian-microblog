7b18b58eb6547914809fcdf9351a6661ec8ceed8
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2022-10-27 20:23:02 +0200

    Add settings documentation

--- src/stores/StoredSettings.ts
+++ src/stores/StoredSettings.ts
@@ -2,11 +2,25 @@
  * Plugin settings.
  */
 export interface StoredSettings {
+    // Application token used to access Micro.blog.
     appToken: string
+
+    // Default tags (set in Settings) that applies
+    // to all new posts.
     tags: string
+
+    // Default post visibility (set in Settings)
+    // that applies to all new posts.
     postVisibility: string,
+
+    // List of blogs available for the given app token.
     blogs: Record<string, string>,
+
+    // Default blog used for new posts.
     selectedBlogID: string,
+
+    // List of tag suggestions for new posts,
+    // retrieved from Micro.blog for the selected blog.
     tagSuggestions: Array<string>
 }
 
