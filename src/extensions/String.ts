c7aa0f5b78df5590045463cee45afaeea72544b4
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2023-08-30 11:11:01 +0200

    Simplify string extension

--- src/extensions/String.ts
+++ src/extensions/String.ts
@@ -4,7 +4,7 @@ declare global {
     }
 }
 
-String.prototype.removeFrontmatter = function(): string {
+String.prototype.removeFrontmatter = function() {
     const regex = /---\s*[\s\S]*?\s*---/
     return this.replace(regex, "")
 }
