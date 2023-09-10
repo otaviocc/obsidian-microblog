62fd6f36775337ef06874c3206ed0c0e4f9110d4
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2023-09-10 09:18:25 +0200

    Add documentation

--- src/extensions/TypeGuards.ts
+++ src/extensions/TypeGuards.ts
@@ -2,14 +2,17 @@ import { PublishViewModel } from '@views/PublishViewModel'
 import { UpdateViewModel } from '@views/UpdateViewModel'
 import { MarkdownView } from 'obsidian'
 
+// Checks if the type is `PublishViewModel`
 export function isPublishViewModel(value: unknown): value is PublishViewModel {
     return typeof value === "object" && value instanceof PublishViewModel
 }
 
+// Checks if the type is `UpdateViewModel`
 export function isUpdateViewModel(value: unknown): value is UpdateViewModel {
     return typeof value === "object" && value instanceof UpdateViewModel
 }
 
+// Checks if the type is `MarkdownView`
 export function isMarkdownView(value: unknown): value is MarkdownView {
     return typeof value === "object" && value instanceof MarkdownView
 }
