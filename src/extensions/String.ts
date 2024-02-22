0146d6cc2a83e950602c922a90696ce8a4dec1cb
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2024-02-22 15:13:47 +0100

    Add String.prototype.flatSplit

--- src/extensions/String.ts
+++ src/extensions/String.ts
@@ -1,12 +1,20 @@
 declare global {
     export interface String {
         removeFrontmatter(): string
+        flatSplit(): string[]
     }
 }
 
 String.prototype.removeFrontmatter = function(this: string) {
     const regex = /---\s*[\s\S]*?\s*---/
     return this.replace(regex, '')
+}
+
+String.prototype.flatSplit = function(this: string) {
+    return this
+        .split(',')
+        .filter(value => value.length > 0)
+        .map(tag => tag.trim())
 }
 
 export { }
